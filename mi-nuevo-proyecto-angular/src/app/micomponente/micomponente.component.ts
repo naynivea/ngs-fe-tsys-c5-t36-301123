caf0824fb54ec1e75e98bad27135d95daa5b1e2f
Nayara <nay.nivea@gmail.com>
2023-12-04 17:30:53 +0100

    Mi primer proyecto angular

---
+++ mi-nuevo-proyecto-angular/src/app/micomponente/micomponente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-micomponente',
+  standalone: true,
+  imports: [],
+  templateUrl: './micomponente.component.html',
+  styleUrl: './micomponente.component.css'
+})
+export class MicomponenteComponent {
+
+}
